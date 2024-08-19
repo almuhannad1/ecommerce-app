d91856e3c0edaf5785c3491bcffdd573f5780776
AL MUHANNAD <almahanad1@gmail.com>
2024-08-19 14:58:30 +0400

    Complete information details page for specific product

---
+++ app/product-details/[productId]/_components/ProductBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+function ProductBanner({ product }) {
+  return (
+    <div>
+      <Image src={product?.attributes?.banner?.data?.attributes?.url} alt='product-details' width={400} height={400} className='rounded-lg' />
+    </div>
+  )
+}
+
+export default ProductBanner
--- app/product-details/[productId]/page.js
+++ app/product-details/[productId]/page.js
@@ -1,21 +1,34 @@
 'use client'
 import BreadCrumb from '@/app/_components/BreadCrumb';
 import ProductApis from '@/app/_utils/ProductApis'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductBanner from './_components/ProductBanner';
+import ProductInfo from './_components/ProductInfo';
 
 function ProductDetails({ params }) {
+    const [productDetails, setProductDetails] = useState({})
+
     useEffect(() => {
         getProductById_();
     }, [params?.productId])
 
     const getProductById_ = () => {
         ProductApis.getProductById(params?.productId)
-            .then(res => console.log(res.data.data))
+            .then(res => {
+                console.log(res.data.data)
+                setProductDetails(res.data.data)
+            })
     }
 
     return (
         <div className='px-10 md:px-28 py-8'>
             <BreadCrumb />
+            {/*  */}
+            <div className='grid grid-cols-1 sm:grid-cols-2 mt-10 gap-5 sm:gap-0 md:flex-row justify-around'>
+                <ProductBanner product={productDetails} />
+                <ProductInfo product={productDetails} />
+            </div>
+            {/*  */}
         </div>
     )
 }
