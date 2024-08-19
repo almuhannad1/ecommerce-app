a23e4bbb9146d2322e456556bf6cc833d9d1d946
AL MUHANNAD <almahanad1@gmail.com>
2024-08-19 11:52:26 +0400

    Complete Product details using id

--- app/product-details/[productId]/page.js
+++ app/product-details/[productId]/page.js
@@ -1,6 +1,17 @@
-import React from 'react'
+'use client'
+import ProductApis from '@/app/_utils/ProductApis'
+import React, { useEffect } from 'react'
 
 function ProductDetails({ params }) {
+    useEffect(() => {
+        getProductById_();
+    }, [params?.productId])
+
+    const getProductById_ = () => {
+        ProductApis.getProductById(params?.productId)
+            .then(res => console.log(res.data.data))
+    }
+
     return (
         <div>{params?.productId}</div>
     )
