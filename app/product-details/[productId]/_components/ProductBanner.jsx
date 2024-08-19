e4f50eb60ebc7df38ac554873bf562ef08222c82
AL MUHANNAD <almahanad1@gmail.com>
2024-08-19 15:49:41 +0400

    Add skeleton effect for product banner

--- app/product-details/[productId]/_components/ProductBanner.jsx
+++ app/product-details/[productId]/_components/ProductBanner.jsx
@@ -4,7 +4,12 @@ import Image from 'next/image'
 function ProductBanner({ product }) {
   return (
     <div>
-      <Image src={product?.attributes?.banner?.data?.attributes?.url} alt='product-details' width={400} height={400} className='rounded-lg' />
+      {
+        product?.attributes?.banner?.data?.attributes?.url ?
+          <Image src={product?.attributes?.banner?.data?.attributes?.url} alt='product-details' width={400} height={400} className='rounded-lg' />
+          :
+          <div className='w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse'></div>
+      }
     </div>
   )
 }
