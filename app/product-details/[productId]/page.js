44ede499a97721b58292e214c490bd6e31e6c500
AL MUHANNAD <almahanad1@gmail.com>
2024-08-19 15:21:50 +0400

    Add similar products list in product details page

--- app/product-details/[productId]/page.js
+++ app/product-details/[productId]/page.js
@@ -4,9 +4,11 @@ import ProductApis from '@/app/_utils/ProductApis'
 import React, { useEffect, useState } from 'react'
 import ProductBanner from './_components/ProductBanner';
 import ProductInfo from './_components/ProductInfo';
+import ProductList from '@/app/_components/ProductList';
 
 function ProductDetails({ params }) {
     const [productDetails, setProductDetails] = useState({})
+    const [productList, setProductList] = useState([])
 
     useEffect(() => {
         getProductById_();
@@ -17,18 +19,32 @@ function ProductDetails({ params }) {
             .then(res => {
                 console.log(res.data.data)
                 setProductDetails(res.data.data)
+                getProductByCategory_(res.data.data)
             })
     }
 
+    const getProductByCategory_ = (product) => {
+        ProductApis.getProductByCategory(product?.attributes?.category)
+            .then(res => {
+                console.log(res?.data?.data)
+                setProductList(res?.data?.data)
+            })
+    }
     return (
         <div className='px-10 md:px-28 py-8'>
             <BreadCrumb />
-            {/*  */}
+            {/* Product Banner & Product Info */}
             <div className='grid grid-cols-1 sm:grid-cols-2 mt-10 gap-5 sm:gap-0 md:flex-row justify-around'>
                 <ProductBanner product={productDetails} />
                 <ProductInfo product={productDetails} />
             </div>
+
             {/*  */}
+            <div>
+                <h2 className='mt-24 text-xl mb-5'>Similar Products</h2>
+                <ProductList productList={productList} />
+            </div>
+
         </div>
     )
 }
